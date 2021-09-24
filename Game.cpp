b404a046b4ab8549914e12684da6d18bf9eec20d
20201287강민혁 <83389456+Land0116@users.noreply.github.com>
2021-09-24 02:04:38 +0000

    실습 4주차 - 원본상자,대상상자, 애니메이션 스프라이트

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,9 @@
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
-  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+  //SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+  //[ 4주차 실습 - 애니메이션 스프라이트 ]
+  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
@@ -19,13 +21,50 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
         {
           m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
           SDL_FreeSurface(pTempSurface);
-          SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+          //SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+          //[ 4주차 실습 - 애니메이션 스프라이트 ]
+          m_sourceRectangle.w = 128;
+          m_sourceRectangle.h = 82;
+          m_sourceRectangle.x = 0;
+          m_sourceRectangle.y = 0;
+
+        /* [4주차 응용 실습 - 1]
+          m_destinationRectangle.w = m_sourceRectangle.w = 50;
+          m_destinationRectangle.h = m_sourceRectangle.h = 50;*/
+
+        //[ 4주차 응용 실습 - 2]
+        /*m_destinationRectangle.w = m_sourceRectangle.w = 50;
+          m_destinationRectangle.h = m_sourceRectangle.h = 50;
+
+          m_destinationRectangle.x = 170;            m_destinationRectangle.y = 130;*/
+        //[ 4주차 응용 실습 - 3]
+        /*m_sourceRectangle.x = 51;
+          m_sourceRectangle.y = 50;
+
+          m_destinationRectangle.w = m_sourceRectangle.w = 50;
+          m_destinationRectangle.h = m_sourceRectangle.h = 50;
+
+          m_destinationRectangle.x = 160;
+          m_destinationRectangle.y = 130;*/
+
+        //[ 4주차 응용 실습 - 4]
+        /*m_destinationRectangle.w = m_sourceRectangle.w = 480;
+          m_destinationRectangle.h = m_sourceRectangle.h = 640;
+
+          m_destinationRectangle.x = 0;
+          m_destinationRectangle.y = 0;*/
+
 
           m_destinationRectangle.w = m_sourceRectangle.w;
           m_destinationRectangle.h = m_sourceRectangle.h;
 
-          m_destinationRectangle.x = m_sourceRectangle.x = 0;
-          m_destinationRectangle.y = m_sourceRectangle.y = 0;
+          m_destinationRectangle.x = 0;
+          m_destinationRectangle.y = 0;
+
+
+          /*m_destinationRectangle.x = m_sourceRectangle.x = 0;
+          m_destinationRectangle.y = m_sourceRectangle.y = 0;*/
+
         }
         else
         {
@@ -53,7 +92,8 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 
 void Game::update()
 {
-  
+  //[ 4주차 실습 - 애니메이션 스프라이트 ]
+  m_sourceRectangle.x = 128 * ((SDL_GetTicks() / 100) % 6);
 }
 
 void Game::render()
@@ -138,6 +178,8 @@ void Game::handleEvents()
             break;
           }
         }
+        default:
+			    break;
       }   
 		}
 		default:
