3905b99f521ee2448728c6ee68cf1b67bee03469
20201287강민혁 <83389456+Land0116@users.noreply.github.com>
2021-11-02 14:28:32 +0000

    Vector2D

--- Enemy.cpp
+++ Enemy.cpp
@@ -9,10 +9,9 @@ void Enemy::draw()
 
 void Enemy::update()
 {
-  m_x += 1;
-  m_y += 1;
+  m_posion.setX(m_posion.getX() + 1);
+  m_posion.setY(m_posion.getY() + 1);
   m_currentFrame = ( (SDL_GetTicks() / 100) % 6);
-  
 }
 
 void Enemy::clean() {}
